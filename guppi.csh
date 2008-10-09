42569768ebd52ec4472ada8c60f4dbf2a1df8b55
Paul Demorest <pdemores@nrao.edu>
2008-10-09 14:11:43 -0400

    Merged Scott's new scripts.   Added guppi_reset_shmem ------------------------------------------------------------ Use --include-merges or -n0 to see merged revisions.

--- guppi.csh
+++ guppi.csh
@@ -4,7 +4,7 @@ set opt64=/opt/64bit
 setenv GUPPI_DIR $opt64/guppi/guppi_daq
 setenv PRESTO $opt64/presto
 setenv PATH $opt64/bin:$GUPPI_DIR/bin:$PRESTO/bin:$PATH
-setenv PYTHONPATH $opt64/lib/python:$opt64/lib/python/site-packages:$opt64/presto/lib/python:$GUPPI_DIR/python
+setenv PYTHONPATH $opt64/lib/python:$opt64/lib/python/site-packages:$opt64/presto/lib/python:$GUPPI_DIR/python:/data1/demorest/rpc/src
 setenv PGPLOT_DIR $opt64/pgplot
 setenv LD_LIBRARY_PATH $opt64/lib:$opt64/pgplot:$opt64/presto/lib
 setenv TEMPO $opt64/tempo
--- python/guppi_set_params.py
+++ python/guppi_set_params.py
@@ -1,42 +1,67 @@
+import os
 from guppi_utils import *
 from astro_utils import current_MJD
 from optparse import OptionParser
 
 # Parse command line
 par = OptionParser()
-par.add_option("-s", "--src", dest="src", help="Set observed source name",
-        action="store", default="Fake_PSR")
-par.add_option("-r", "--ra", dest="ra", help="Set source R.A. (hh:mm:ss.s)",
-        action="store", default="12:34:56.7")
-par.add_option("-d", "--dec", dest="dec", help="Set source Dec (+/-dd:mm:ss.s)",
-        action="store", default="+12:34:56.7")
-par.add_option("-f", "--freq", dest="freq", help="Set center freq (MHz)",
-        action="store", type="float", default=1200.0)
-par.add_option("-n", "--scannum", dest="scan", help="Set scan number",
-        action="store", type="int", default=1);
-par.add_option("-I", "--increment_scan", dest="inc", help="Increment scan num",
-        action="store_true", default=False);
-par.add_option("-l", "--length", dest="len", help="Scan length",
-        action="store", type="float", default=3600.0)
-par.add_option("-a", "--acc_len", dest="acc_len", help="Accumulation length",
-        action="store", type="int", default=16)
-par.add_option("-c", "--cal", dest="cal", help="Setup for cal scan",
-        action="store_true", default=False)
-par.add_option("-4", "--gb43m", dest="gb43m", 
-        help="Set params for 43m observing",
-        action="store_true", default=False)
-par.add_option("-F", "--fake", dest="fake", help="Set params for fake psr",
-        action="store_true", default=False)
-par.add_option("-g", "--gbt", dest="gbt", 
-        help="Use values from gbtstatus",
-        action="store_true", default=True)
+par.add_option("-c", "--cal", dest="cal",
+               help="Setup for cal scan (folding mode)",
+               action="store_true", default=False)
+par.add_option("-n", "--scannum", dest="scan",
+               help="Set scan number",
+               action="store", type="int", default=1);
+par.add_option("-i", "--increment_scan", dest="inc",
+               help="Increment scan num",
+               action="store_true", default=False);
+par.add_option("-T", "--tscan", dest="tscan",
+               help="Scan length (sec)",
+               action="store", type="float", default=8*3600.0)
 par.add_option("-P", "--parfile", dest="parfile", 
-        help="Use this parfile for folding",
-        action="store", default="");
-par.add_option("-i", "--tfold", dest="tfold", help="Fold dump time",
-        action="store", type="float", default=30.0)
-par.add_option("-b", "--bins", dest="nbin", help="Number of profile bins",
-        action="store", type="int", default=256)
+               help="Use this parfile for folding",
+               action="store", default="");
+par.add_option("-t", "--tfold", dest="tfold",
+               help="Fold dump time (sec)",
+               action="store", type="float", default=30.0)
+par.add_option("-b", "--bins", dest="nbin",
+               help="Number of profile bins for folding",
+               action="store", type="int", default=256)
+par.add_option("-I", "--onlyI", dest="onlyI",
+               help="Only record total intensity",
+               action="store_true", default=False)
+par.add_option("--dstime", dest="dstime",
+               help="Downsample in time (int, power of 2)",
+               action="store", type="int", default=1)
+par.add_option("--dsfreq", dest="dsfreq",
+               help="Downsample in freq (int, power of 2)",
+               action="store", type="int", default=1)
+par.add_option("--obs", dest="obs",
+               help="Set observers name",
+               action="store", default="unknown")
+par.add_option("--src", dest="src",
+               help="Set observed source name",
+               action="store", default="Fake_PSR")
+par.add_option("--ra", dest="ra",
+               help="Set source R.A. (hh:mm:ss.s)",
+               action="store", default="12:34:56.7")
+par.add_option("--dec", dest="dec",
+               help="Set source Dec (+/-dd:mm:ss.s)",
+               action="store", default="+12:34:56.7")
+par.add_option("--freq", dest="freq",
+               help="Set center freq (MHz)",
+               action="store", type="float", default=1200.0)
+par.add_option("--acc_len", dest="acc_len",
+               help="Hardware accumulation length",
+               action="store", type="int", default=16)
+par.add_option("--gb43m", dest="gb43m", 
+               help="Set params for 43m observing",
+               action="store_true", default=False)
+par.add_option("--gbt", dest="gbt", 
+               help="Use values from gbtstatus (the default)",
+               action="store_true", default=True)
+par.add_option("--fake", dest="fake",
+               help="Set params for fake psr",
+               action="store_true", default=False)
 (opt,arg) = par.parse_args()
 
 g = guppi_status()
@@ -45,6 +70,14 @@ g.read()
 
 if (opt.inc):
     opt.scan = g["SCANNUM"] + 1
+
+if opt.obs=="unknown":
+    try:
+        username = os.environ['LOGNAME']
+    except KeyError:
+        username = os.getlogin()
+    else:
+        username = "unknown"
 
 g.update("SCANNUM", opt.scan)
 
@@ -57,7 +90,6 @@ if (opt.gbt):
     g.update_with_gbtstatus()
     g.update("OBSBW", 800.0)
 else:
-    g.update("OBSERVER", "GUPPI Crew")
     g.update("FRONTEND", "None")
     g.update("PROJID", "GUPPI tests")
     g.update("FD_POLN", "LIN")
@@ -66,6 +98,9 @@ else:
     g.update("RA_STR", opt.ra)
     g.update("DEC_STR", opt.dec)
     g.update("OBSFREQ", opt.freq)
+
+if g['OBSERVER']=='unknown':
+    g.update("OBSERVER", username)
 
 if (1):  # in case we don't get a real start time
     MJD = current_MJD()
@@ -80,17 +115,20 @@ if (1):  # in case we don't get a real start time
 
 g.write()
 
+g.update("TFOLD", opt.tfold)
 if (opt.cal):
     g.update("OBS_MODE", "CAL")
-    g.update("SCANLEN", opt.len)
+    if opt.tscan > 7.9*3600:
+        opt.tscan = 60.0
+    g.update("TFOLD", 10.0) # override for cal scans
     g.update("BASENAME", "guppi_%5d_%s_%04d_cal"%(g['STT_IMJD'], g['SRC_NAME'], g['SCANNUM']))
     g.update("CAL_MODE", "ON")
 else:
     g.update("OBS_MODE", "SEARCH")
-    g.update("SCANLEN", opt.len)
     g.update("BASENAME", "guppi_%5d_%s_%04d"%(g['STT_IMJD'], g['SRC_NAME'], g['SCANNUM']))
     g.update("CAL_MODE", "OFF")
 
+g.update("SCANLEN", opt.tscan)
 g.update("BACKEND", "GUPPI")
 g.update("PKTFMT", "GUPPI")
 g.update("DATAHOST", "bee2_10")
@@ -110,12 +148,14 @@ g.update("ACC_LEN", opt.acc_len)
 
 g.update("NRCVR", 2)
 
-g.update("ONLY_I", 0)
-g.update("DS_TIME", 1)
-g.update("DS_FREQ", 1)
+if opt.onlyI:
+    g.update("ONLY_I", 1)
+else:
+    g.update("ONLY_I", 0)
+g.update("DS_TIME", opt.dstime)
+g.update("DS_FREQ", opt.dsfreq)
 
 g.update("NBIN", opt.nbin)
-g.update("TFOLD", opt.tfold)
 g.update("PARFILE", opt.parfile)
 
 #g.update("BLOCSIZE", )
