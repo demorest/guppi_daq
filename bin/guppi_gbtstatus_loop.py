35977f74a6cea81df246aa8bdb3a761fa73c43b2
Scott Ransom <sransom@nrao.edu>
2009-07-22 17:49:43 -0400

    Removed code to re-compute STT_* variables which should only be   set at scan start by guppi_net_thread.

--- bin/guppi_gbtstatus_loop.py
+++ bin/guppi_gbtstatus_loop.py
@@ -9,20 +9,5 @@ g = guppi_status()
 while (1):
     g.read()
     g.update_with_gbtstatus()
-
-    # Current time
-    MJD = current_MJD()
-    MJDd = int(MJD)
-    MJDf = MJD - MJDd
-    MJDs = int(MJDf * 86400 + 1e-6)
-    offs = (MJD - MJDd - MJDs/86400.0) * 86400.0
-    g.update("STT_IMJD", MJDd)
-    g.update("STT_SMJD", MJDs)
-    if offs < 2e-6: offs = 0.0
-    g.update("STT_OFFS", offs)
-    
-    # Apply to shared mem
     g.write()
-    
-    # Sleep for 1 second
     time.sleep(1)
